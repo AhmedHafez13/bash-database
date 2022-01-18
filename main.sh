772f0aaa2a4be8497d605b85bc3a0f11b2985d8b
AymanxMohamed <aymanmido067@gmail.com>
2022-01-18 21:45:17 +0200

    adding listDB, dropDB, clearScreen functions

--- main.sh
+++ main.sh
@@ -14,13 +14,15 @@ createMainMenu
 # Create main Menu Function
 function createMainMenu 
 {
+    echo "===========================================";
     echo -e "#_____ * _____ * Main Menu _____ * _____ #";
     
     echo "1) Create Database";
     echo "2) List Databases";
     echo "3) Connect To Databases";
     echo "4) Drop Database";
-    echo "5) Exit";
+    echo "5) clear Screen"; 
+    echo "6) Exit";
     echo "===========================================";
     echo -e "Please Enter your choice: \c"
     read ch
@@ -29,10 +31,16 @@ function createMainMenu
       2)  listDB ;;
       3)  connectToDB ;;
       4)  dropDB ;;
-      5)  exit ;;
-      *)  echo "Please select a correct number from the given menu"; createMainMenu;
+      5)  clearScreen ;;
+      6)  exit ;;
+      *)  echo "Please select a correct number from the given menu";createMainMenu;
     esac
 }
+
+
+
+# _____ * _____ Main Menu Functions  _____ * _____ #
+
 
 # a function that create a database as a folder relative to the script
 function createDB 
@@ -42,11 +50,48 @@ function createDB
   mkdir ./AHDB/$dbName
   if [[ $? == 0 ]]
   then
-    echo "$dbName Database Created Successfully"
+    echo "$dbName Database Created Successfully";
   else
-    echo "Error Creating the database $dbName"
+    echo "Error Creating the database $dbName"; 
   fi
-  echo "==========================================";
-  createMainMenu
+  #echo "==========================================";
+  createMainMenu;
 }
 
+# function to list the database
+function listDB
+{
+  ls ./AHDB; 
+  #echo "==========================================";
+  createMainMenu;
+}
+
+#function to connect to the database
+# function connectToDB
+# {
+#   # todo
+# }
+
+# a function that remove the database folder 
+function dropDB 
+{
+  echo -e "Please Enter Database Name: \c";
+  read dbName;
+  rm -r ./AHDB/$dbName 2>> ./.error.log;
+  if [[ $? == 0 ]]
+  then
+    echo "$dbName Dropped Successfully";
+  else
+    echo "$dbName dosn't exists!";
+  fi
+  #echo "==========================================";
+  createMainMenu;
+}
+
+#clear screen Function
+function clearScreen
+{
+  clear;
+  createMainMenu;
+}
+# _____ * _____ End of  Main Menu Functions  _____ * _____ #
